24d4756dbba02890140a33d4a12ac1c6db30dab4
Dmytro Petrenko <blinker182ca@gmail.com>
2020-10-17 14:51:48 +0300

    clear file from code
    add imports

--- src/index.js
+++ src/index.js
@@ -1,39 +1,5 @@
 // import hbs from "./templates/menu-items";
 // console.log(hbs);
-import './css/styles.css'
-
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
-
-const refs = {
-    body: document.querySelector('body'),
-    themeToggle: document.querySelector('#theme-switch-toggle'),
-}
-
-refs.themeToggle.addEventListener('change', onThemeColorChange);
-
-function onThemeColorChange(e) {
-  // console.log(e.target.checked);
-  const themeColor = e.target.checked ? Theme.DARK : Theme.LIGHT;
-  localStorage.setItem("theme", themeColor);
-
-  updateThemeColor();
-}
-
-function updateThemeColor(e) {
-  const currentThemeColor = localStorage.getItem('theme');
-
-  if (currentThemeColor === Theme.DARK) {
-    refs.body.classList.remove(Theme.LIGHT);
-    refs.body.classList.add(Theme.DARK);
-    refs.themeToggle.checked = true;
-  } else {
-    refs.body.classList.remove(Theme.DARK);
-    refs.body.classList.add(Theme.LIGHT);
-  }
-}
-
-updateThemeColor();
-
+import './css/styles.css';
+import * as menuMarkup from './js/menu-items';
+import * as themeToggle from './js/theme-toggle';
