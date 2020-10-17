ff57fa82f45a3ab75ebe6fea685b8b69ea602643
Dmytro Petrenko <blinker182ca@gmail.com>
2020-10-17 14:53:58 +0300

    moved  code to an external file
    create theme-toggle.js

---
+++ src/js/theme-toggle.js
@@ -0,0 +1,34 @@
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+    body: document.querySelector('body'),
+    themeToggle: document.querySelector('#theme-switch-toggle'),
+}
+
+refs.themeToggle.addEventListener('change', onThemeColorChange);
+
+function onThemeColorChange(e) {
+  // console.log(e.target.checked);
+  const themeColor = e.target.checked ? Theme.DARK : Theme.LIGHT;
+  localStorage.setItem("theme", themeColor);
+
+  updateThemeColor();
+}
+
+function updateThemeColor() {
+  const currentThemeColor = localStorage.getItem('theme');
+
+  if (currentThemeColor === Theme.DARK) {
+    refs.body.classList.remove(Theme.LIGHT);
+    refs.body.classList.add(Theme.DARK);
+    refs.themeToggle.checked = true;
+  } else {
+    refs.body.classList.remove(Theme.DARK);
+    refs.body.classList.add(Theme.LIGHT);
+  }
+}
+
+updateThemeColor();
